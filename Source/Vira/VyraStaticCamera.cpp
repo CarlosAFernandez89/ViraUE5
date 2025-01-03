0f0b4dfa5b56889333c90d93d881d12a331b2f3f
RelaxThatRepo <91145504+CarlosAFernandez89@users.noreply.github.com>
2025-01-03 08:59:48 -0800

    Static camera trigger.
    Changed fireball distance to allow screen wide attack.
    Change location of Charm Effect from the ranged ability to the actual projectile due to bug that prevented events to bind if fireball was destroyed too fast.

---
+++ Source/Vira/VyraStaticCamera.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VyraStaticCamera.h"
+
+#include "Character/VyraPlayerStateCharacter.h"
+#include "Components/BoxComponent.h"
+#include "Messages/VyraVerbMessageHelpers.h"
+
+
+AVyraStaticCamera::AVyraStaticCamera()
+{
+	OverlapBoxComponent = CreateDefaultSubobject<UBoxComponent>("OverlapBoxComponent");
+	OverlapBoxComponent->SetupAttachment(RootComponent);
+	OverlapBoxComponent->SetGenerateOverlapEvents(true);
+	OverlapBoxComponent->SetLineThickness(15.f);
+	OverlapBoxComponent->SetBoxExtent(FVector(200.f, 500.f, 500.f));
+
+	OverlapBoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AVyraStaticCamera::OnOverlapBegin_OverlapBoxComponent);
+	OverlapBoxComponent->OnComponentEndOverlap.AddDynamic(this, &AVyraStaticCamera::OnOverlapEnd_OverlapBoxComponent);
+	
+}
+
+void AVyraStaticCamera::OnOverlapBegin_OverlapBoxComponent(UPrimitiveComponent* OverlapComponent, AActor* OtherActor,
+                                                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA(AVyraPlayerStateCharacter::StaticClass()) && NewViewTarget)
+	{
+		if (APlayerController* PC = UVyraVerbMessageHelpers::GetPlayerControllerFromObject(OtherActor))
+		{
+			PC->SetViewTargetWithBlend(NewViewTarget, BlendTime, VTBlend_Cubic);
+		}
+	}
+}
+
+void AVyraStaticCamera::OnOverlapEnd_OverlapBoxComponent(UPrimitiveComponent* OverlapComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor->IsA(AVyraPlayerStateCharacter::StaticClass()) && NewViewTarget)
+	{
+		if (APlayerController* PC = UVyraVerbMessageHelpers::GetPlayerControllerFromObject(OtherActor))
+		{
+			PC->SetViewTargetWithBlend(OtherActor, BlendTime, VTBlend_Cubic);
+		}
+	}
+}
---
+++ Source/Vira/VyraStaticCamera.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VyraStaticCamera.generated.h"
+
+UCLASS()
+class VIRA_API AVyraStaticCamera : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AVyraStaticCamera();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Components")
+	class UBoxComponent* OverlapBoxComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = "ViewTarget")
+	AActor* NewViewTarget = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,Category = "ViewTarget")
+	float BlendTime = 0.5f;
+	
+
+	UFUNCTION()
+	void OnOverlapBegin_OverlapBoxComponent(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd_OverlapBoxComponent(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
